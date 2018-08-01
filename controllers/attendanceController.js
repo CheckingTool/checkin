419b2c679a68299a39815af4aff09bf1d566b155
Nikita <nikita.pismenniy@together.com>
2018-08-01 17:59:48 +0300

    #attendance // refactoring

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -19,8 +19,8 @@ connection.connect(function(err) {
 let Controller = function(){};
 var lessonName = [];
 var lessonID = [];
-var selDate;
-var selGroup;
+//var selDate;
+//var selGroup;
 var groupID;
 var studArr = [];
 
@@ -32,46 +32,104 @@ Controller.index = async function(req, res) {
 
 };
 
-Controller.list = function(req, res, next) {
-    console.log('controller list');
-    selDate = req.body.date;
-    selGroup = req.body.group;
-    var selGroupID;
-    console.log(selDate);
-    if (selDate == 'Total') {
-        connection.query('select students.name, students.Total_Misses, groups.ID from students inner join groups on students.Group_ID=groups.ID WHERE groups.Name = ? ', [selGroup], function(err, results) {
-           if (err) {
-               console.log(err);
-           } else {
-               console.log(results);
-               res.render('attendancetotal', {students: results, group: selGroup, date: selDate});
-           }
-        });
-    } 
-    else {
-        connection.query('SELECT ID FROM groups WHERE Name=?', [selGroup], function(err, results) {
-            if (err) {
-                console.log(err);
-            } else {
-                for (var i in results) {
-                    selGroupID = results[i].ID;
-                }
-                connection.query('select student_misses.Lesson_ID, student_misses.Student_ID, student_misses.Attend, student_misses.Date, students.Name, lessons.Name as SubjName from student_misses inner join students on student_misses.Student_ID=students.ID inner join lessons on student_misses.Lesson_ID=lessons.ID WHERE students.Group_ID = ? and student_misses.Date = ?', [selGroupID, selDate], function(err, results) {
-                   if (err) {
-                       console.log(err);
-                   } else {
-                       for (var i in results) {
-                           console.log(results[i]);
-                           studArr.push(results[i]);
-                       }
-                       console.log('studarr:', studArr.Name, 'length: ', studArr.length);
-                       res.render('attendancelist', {students: studArr, group: selGroup, date: selDate});
-                       studArr = [];
-                   }
-                });
+Controller.list = async function(req, res) {
+    const selectedDate = req.body.date;
+    const selectedGroup = req.body.group;
+    let students;
+
+    if (selectedDate === "Total") {
+      students = await Controller.getTotalStudents(selectedGroup);
+    } else {
+      students = await Controller.getStudents(selectedGroup, selectedDate);
+    }
+
+    connection.query('SELECT ID FROM groups WHERE Name=?', [selectedGroup], function(err, results) {
+      let selectedGroupID;
+
+        if (err) {
+            console.log(err);
+        } else {
+          console.log("RESULTS: ", results);
+            for (var i in results) {
+                selectedGroupID = results[i].ID;
             }
-        });
-    }   
+            connection.query(
+              'SELECT student_misses.Lesson_ID, ' +
+              'student_misses.Student_ID, ' +
+              'student_misses.Attend, ' +
+              'student_misses.Date, ' +
+              'students.Name, ' +
+              'lessons.Name as SubjName ' +
+              'FROM student_misses INNER JOIN students ' +
+              'ON student_misses.Student_ID=students.ID ' +
+              'INNER JOIN lessons on student_misses.Lesson_ID=lessons.ID ' +
+              'WHERE students.Group_ID = ? AND student_misses.Date = ?', [selectedGroupID, selectedDate],
+              (err, results) => {
+               if (err) {
+                   console.log(err);
+               } else {
+                 console.log("RESULLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL", results);
+                   for (var i in results) {
+                       console.log(results[i]);
+                       studArr.push(results[i]);
+                   }
+                   res.render('attendancelist', {students: studArr, group: selectedGroup, date: selectedDate});
+                   studArr = [];
+               }
+            });
+        }
+    });
+};
+
+
+Controller.getTotalStudents = async function(group) {
+
+  return new Promise((resolve, reject) => {
+    connection.query(`select students.name, students.Total_Misses, groups.ID 
+      FROM students INNER JOIN groups 
+      ON students.Group_ID=groups.ID WHERE groups.Name = ? `, [group],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+
+        resolve(result);
+
+    });
+  })
+};
+
+Controller.getStudents = async function(group, date) {
+
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT ID FROM groups WHERE Name=?", [group], (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return result[0].ID;
+    })
+    .then(id => {
+      connection.query(`SELECT student_misses.Lesson_ID,
+        student_misses.Student_ID,
+        student_misses.Attend, 
+        student_misses.Date,
+        students.Name,
+        lessons.Name as SubjName 
+        FROM student_misses INNER JOIN students 
+        ON student_misses.Student_ID=students.ID 
+        INNER JOIN lessons on student_misses.Lesson_ID=lessons.ID
+        WHERE students.Group_ID = ? AND student_misses.Date = ?`, [id, date], (err, result) => {
+
+        if (err) {
+          return reject(err);
+        }
+
+      });
+    })
+
+  })
 };
 
 Controller.getGroups = async function() {
