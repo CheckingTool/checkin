b3505cfe62cd4525146c9b848c85cdec8afcc34d
Nikita <nikita.pismenniy@together.com>
2018-08-03 17:01:27 +0300

    #checklist // refactoring

--- controllers/checklistController.js
+++ controllers/checklistController.js
@@ -30,7 +30,6 @@ var attendance = [];
 var success = false;
 
 Controller.index = async function(req, res){
-
   const groups = await Controller.getGroups();
   const rawDates = await Controller.getDates();
   const uniqDates = _.uniqBy(rawDates, "Date");
@@ -38,138 +37,83 @@ Controller.index = async function(req, res){
   res.render('checklist', { groups: groups, dates: uniqDates });
 };
 
-Controller.schedule = function(req, res, next) {
-    lessonName = [];
-    const groupName = req.body.group;
-    const date = req.body.date;
+Controller.schedule = async function(req, res) {
+  const groupName = req.body.group;
+  const date = req.body.date;
 
-    selGroup = groupName;
-    selDate = date;
+  const groupID = await Controller.getSelectedGroupID(groupName);
+  const lessonsID = await Controller.getLessonsID(groupID, date);
+  const selectedLessonsNames = await Controller.getSelectedLessons(lessonsID);
+  const students = await Controller.getStudents(groupID);
 
-    connection.query("SELECT * from groups WHERE Name = ?", [groupName], function(err, results) {
-      if (err) {
+  res.render('misses', { lessons: selectedLessonsNames, date: date, group: groupName, students: students });
+
+};
+
+Controller.feedback = async function(req) {
+  const lessonName = req.body.lessonname;
+  const attendanceArr = req.body["studarr[]"];
+
+  const lessonID = await Controller.getFilledLessonID(lessonName);
+
+  connection.query('SELECT ID from students where Group_ID = ?', [groupID], function(err, results) {
+    if (err) {
         console.log(err);
-      }
-
+    } else {
       for (var i in results) {
-        groupID = results[i].ID; //получаем айди группы для дальнейшего поиска по таблице расписания
+          studID.push(results[i].ID);
+          //получаем айди всех студентов выбранной группы
       }
-      connection.query("SELECT Lesson_ID FROM schedules WHERE Group_ID = ? AND Date = ?", [groupID, date], function(err, results, fields) {
+      let temp;
+
+      temp = missesinfo['studarr[]'];
+      for (var i in temp) {
+          attendance.push(temp[i]);
+          //массив отметок посещаемости
+      }
+    }
+    var studObj = {};
+    for (var i=0; i<studID.length; i++) {
+      if (attendance[i] == 'false' ) {
+        connection.query('UPDATE students SET Total_Misses = Total_Misses+1 WHERE ID = ?', [studID[i]], function(err, results) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+      connection.query('INSERT INTO student_misses (Lesson_ID, Student_ID, Date, Attend) VALUES (?, ?, ?, ?)', [lessonID, studID[i], selDate, attendance[i]], function(err, results){
         if (err) {
-            console.log(err);
-        }
-
-        console.log('resulst are: ', results);
-        for (var i in results) {
-            lessonID.push(results[i].Lesson_ID); //получаем айди предметов для данной группы в данный день
-        }
-        
-        connection.query("SELECT Name FROM lessons WHERE ID IN (?)", //получаем массив предметов в lessonname
-            [lessonID], function(err, results, fields) {
-           if (err) {
-               console.log(err);
-           }
-
-           for (var i in results) {
-             lessonName.push(results[i].Name);
-           }
-
-           console.log('lessonname:', lessonName);
-           res.redirect(301, '/checklist/misses');
-
-        });
-      });
-    });
-   
-};
-
-Controller.misses = function(req, res, next) {
-    console.log('controller misses');
-    connection.query('SELECT * from students WHERE Group_ID=?', [groupID], function(err, results) { //получаем всех студентов
-       if (err) {
-           console.log(err);
-       } else {
-           res.render('misses', {lessons: lessonName, date: selDate, group: selGroup, students: results});  
-           lessonName = [];
-           lessonID = [];
-       }
-    });
-    
-};
-
-Controller.feedback = function(req, res, next) {
-  var lessonid;
-  console.log('controller feedback');
-  var missesinfo = req.body;
-    connection.query('SELECT ID from lessons WHERE Name= ?', [missesinfo.lessonname], function(err, results){
-        if (err) { 
-            console.log(err);
+          console.log(err);
         } else {
-            for (var i in results) {
-                lessonid = results[i].ID;
-                //получаем айди выбранного для заполнения посещаемости предмета. его нужно обнулять
-            }
-
+          success = true;
         }
-        connection.query('SELECT ID from students where Group_ID = ?', [groupID], function(err, results) {
-            if (err) {
-                console.log(err);
-            } else {
-                for (var i in results) {
-                    studID.push(results[i].ID);
-                    //получаем айди всех студентов выбранной группы
-                }
-                delete missesinfo.lessonname;
-                let temp;
-                
-                temp = missesinfo['studarr[]'];
-                for (var i in temp) {
-                    attendance.push(temp[i]);
-                    //массив отметок посещаемости
-                }
-            }
-            var studObj = {};
-            for (var i=0; i<studID.length; i++) {
-                    if (attendance[i] == 'false' ) {
-                        connection.query('UPDATE students SET Total_Misses = Total_Misses+1 WHERE ID = ?', [studID[i]], function(err, results) {
-                            if (err) {
-                                console.log(err);
-                            }
-                        });
-                    }
-                    connection.query('INSERT INTO student_misses (Lesson_ID, Student_ID, Date, Attend) VALUES (?, ?, ?, ?)', [lessonid, studID[i], selDate, attendance[i]], function(err, results){
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        success = true;
-                    }
 
-                });
+      });
 
-            }
-            lessonName = [];
-            lessonid = '';
-            lessonID = [];
-            groupName = '';
-            groups = [];
-            studID = [];
-            selGroup = '';
-            dates = [];
-            attendance = [];
-           delete results;
-            
-        });
-    }); 
+    }
+    lessonName = [];
+    lessonid = '';
+    lessonID = [];
+    groupName = '';
+    groups = [];
+    studID = [];
+    selGroup = '';
+    dates = [];
+    attendance = [];
+   delete results;
+
+  });
 
 };
 
 Controller.alert = function(req, res, next) {
-    if (success = true) {
-        res.json({message: 'You have successfully added results'});
-    } else {
-        res.json({message: 'You couldnt add results'});
-    }
-    success = false;
+  if (success) {
+    res.json({ message: 'You have successfully added results' });
+  } else {
+    res.json({ message: 'You couldnt add results' });
+  }
+
+  success = false;
 };
 
 Controller.getGroups = async function() {
@@ -182,6 +126,85 @@ Controller.getGroups = async function() {
       resolve(result);
     });
   })
+};
+
+Controller.getFilledLessonID = async function(lesson) {
+  return new Promise((resolve, reject) => {
+
+    connection.query(`SELECT ID FROM lessons WHERE Name = ?`, [lesson], (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(result[0].ID);
+    })
+  })
+};
+
+Controller.getSelectedGroupID = async function(group) {
+
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM groups WHERE Name = ?`, [group], (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(result[0].ID);
+    })
+  })
+};
+
+Controller.getLessonsID = async function(groupID, date) {
+  return new Promise((resolve, reject) => {
+
+    connection.query(`SELECT Lesson_ID FROM schedules WHERE Group_ID = ? AND Date = ?`, [groupID, date],
+      (err, result) => {
+
+        if (err) {
+          return reject(err);
+        }
+
+        let lessonsID = [];
+
+        result.forEach(lesson => {
+          lessonsID.push(lesson.Lesson_ID);
+        });
+
+        resolve(lessonsID);
+      })
+  })
+};
+
+Controller.getSelectedLessons = async function(lessonsID) {
+
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT Name FROM lessons WHERE ID IN (?)`,
+      [lessonsID], (err, result) => {
+
+        if (err) {
+            return reject(err);
+        }
+
+        let currentLessonsNames = [];
+        result.forEach(lesson => {
+          currentLessonsNames.push(lesson.Name);
+        });
+
+        resolve(currentLessonsNames);
+      });
+  });
+};
+
+Controller.getStudents = async function(groupID) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM students WHERE GROUP_ID = ?`, [groupID], (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(result);
+    })
+  });
 };
 
 Controller.getDates = async function() {
--- routes/checklist.js
+++ routes/checklist.js
@@ -14,7 +14,6 @@ function checkAuth(req, res, next) {
 
 /* GET home page. */
 router.get('/', checkAuth, checklistController.index);
-router.get('/misses', checklistController.misses);
 router.get('/alert', checklistController.alert);
 router.post('/schedule', checklistController.schedule);
 router.post('/feedback', checklistController.feedback);
